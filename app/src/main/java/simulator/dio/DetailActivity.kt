7a30a3b2aa3bf5d49075a70d5bd76db0931510dd
Laryssa <laryssa.beatriz.n@gmail.com>
2022-07-18 12:05:47 -0300

    Criação da tela de Detalhes, modificação do estilo e dos temas

---
+++ app/src/main/java/simulator/dio/DetailActivity.kt
@@ -0,0 +1,20 @@
+package simulator.dio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import simulator.dio.databinding.ActivityDetailBinding
+import simulator.dio.databinding.ActivityDetailBinding.*
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
